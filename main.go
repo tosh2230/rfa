fed735577aaecdef71e744330d7d5601d2912341
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-04-05 22:57:44 +0900

    split main func

--- main.go
+++ main.go
@@ -2,54 +2,9 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-
-	vision "cloud.google.com/go/vision/apiv1"
+	"rfa/vision_api"
 )
 
 func main() {
-	ctx := context.Background()
-
-	// Creates a client.
-	client, err := vision.NewImageAnnotatorClient(ctx)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-	defer client.Close()
-
-	// Sets the name of the image file to annotate.
-	filename := "IMG_0710.PNG"
-
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatalf("Failed to read file: %v", err)
-	}
-	defer file.Close()
-	image, err := vision.NewImageFromReader(file)
-	if err != nil {
-		log.Fatalf("Failed to create image: %v", err)
-	}
-
-	// labels, err := client.DetectLabels(ctx, image, nil, 10)
-	annotations, err := client.DetectTexts(ctx, image, nil, 10)
-	if err != nil {
-		log.Fatalf("Failed to detect labels: %v", err)
-	}
-
-	// fmt.Println("Labels:")
-	// for _, label := range labels {
-	// 	fmt.Println(label.Description)
-	// }
-
-	if len(annotations) == 0 {
-		fmt.Println("No text found.")
-	} else {
-		fmt.Println("Text:")
-		for _, annotation := range annotations {
-			fmt.Println(annotation.Description)
-		}
-	}
+	vision_api.Detect()
 }
---
+++ vision_api/vision_api.go
@@ -0,0 +1,49 @@
+package vision_api
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	vision "cloud.google.com/go/vision/apiv1"
+)
+
+func Detect() {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	// Sets the name of the image file to annotate.
+	filename := "img/IMG_0710.PNG"
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+	defer file.Close()
+	image, err := vision.NewImageFromReader(file)
+	if err != nil {
+		log.Fatalf("Failed to create image: %v", err)
+	}
+
+	// labels, err := client.DetectLabels(ctx, image, nil, 10)
+	annotations, err := client.DetectTexts(ctx, image, nil, 10)
+	if err != nil {
+		log.Fatalf("Failed to detect labels: %v", err)
+	}
+
+	if len(annotations) == 0 {
+		fmt.Println("No text found.")
+	} else {
+		fmt.Println("Text:")
+		for _, annotation := range annotations {
+			fmt.Println(annotation.Description)
+		}
+	}
+}
