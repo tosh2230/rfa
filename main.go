afeeded46c0d6377691d91b470c9565825b23312
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-04-10 20:10:04 +0900

    add table scheme

--- main.go
+++ main.go
@@ -4,11 +4,28 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
 	"rfa/twitter"
 	"rfa/vision_texts"
 	"strconv"
 	"strings"
 )
+
+type Summary struct {
+	TwitterId            string  `json:"twitter_id" csv:"twitter_id"`
+	TotalTimeExcercising string  `json:"total_time_excercising" csv:"total_time_excercising"`
+	TotalCaloriesBurned  float64 `json:"total_calories_burned" csv:"total_calories_burned"`
+	TotalDistanceRun     float64 `json:"total_distance_run" csv:"total_distance_run"`
+	CreatedAt            string  `json:"created_at" csv:"created_at"`
+}
+
+type Details struct {
+	TwitterId     string `json:"twitter_id" csv:"twitter_id"`
+	ExerciseName  string `json:"exercise_name" csv:"exercise_name"`
+	Quantity      int    `json:"quantity" csv:"quantity"`
+	TotalQuantity int    `json:"total_quantity" csv:"total_quantity"`
+	CreatedAt     string `json:"created_at" csv:"created_at"`
+}
 
 func main() {
 	user := flag.String("u", "", "twitter_user_id")
@@ -32,12 +49,57 @@ func main() {
 			fmt.Println(url)
 			if !strings.HasPrefix(texts[0], "本日の運動結果") &&
 				!strings.HasPrefix(texts[0], "Today's Results") {
+				fmt.Println("No images found.")
 				os.Exit(0)
 			}
 
-			for _, line := range strings.Split(texts[0], "\n") {
-				fmt.Println(strings.TrimSpace(strings.Trim(line, "*")))
-			}
+			createCSV(texts)
 		}
 	}
+}
+
+func createCSV(texts []string) {
+	lines := strings.Split(texts[0], "\n")
+	lastWords := lines[len(lines)-2]
+
+	switch {
+	// summary
+	case strings.HasPrefix(lastWords, "次へ"), strings.HasPrefix(lastWords, "Next"):
+		fmt.Println("Summary:")
+		createCsvSummary(lines)
+
+	// details
+	case strings.HasPrefix(lastWords, "とじる"), strings.HasPrefix(lastWords, "Close"):
+		fmt.Println("Details:")
+		createCsvDetails(lines)
+
+	default:
+		fmt.Println("No images found.")
+		os.Exit(0)
+	}
+}
+
+func createCsvSummary(lines []string) {
+	for _, line := range lines {
+		fmt.Println(replaceLine(line))
+	}
+}
+
+func createCsvDetails(lines []string) {
+	var isOdd bool = (len(lines)%2 == 0)
+
+	for i, line := range lines {
+		r := regexp.MustCompile(`[0-9].+`)
+		if isOdd && r.MatchString(lines[i]) && r.MatchString(lines[i+1]) {
+			break
+		} else {
+			fmt.Println(replaceLine(line))
+		}
+	}
+}
+
+func replaceLine(line string) string {
+	rline := strings.TrimSpace(strings.Trim(line, "*"))
+	rline = strings.Replace(rline, "Om(", "0m(", 1)
+	return rline
 }
