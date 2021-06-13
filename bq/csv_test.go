8629d44b5ba3fa038c22fc8d0660acb5a95b3031
Toshifumi Tsutsumi <rev.to12@gmail.com>
2021-06-13 14:11:41 +0900

    update TestReplaceLines

--- bq/csv_test.go
+++ bq/csv_test.go
@@ -7,10 +7,36 @@ import (
 )
 
 func TestReplaceLines(t *testing.T) {
-	out := "0"
-	want := "1"
-	if out != want {
-		err := errors.Errorf("Fail.\n[out ]: %s\n[want]: %s", out, want)
-		t.Error(err)
+	before := []string{
+		"ジョギング",
+		"Om(19809m)",
+		"おなか押しにみひねり",
+		"320(486回)",
+		"バンザイスクワフット",
+		"- 5回(5回)",
+		"モモあげ",
+		"Im(801m)",
+		"ジョギング",
+		"2 m(19809m)",
+	}
+	wants := []string{
+		"ジョギング",
+		"0m(19809m)",
+		"おなか押しこみひねり",
+		"32回(486回)",
+		"バンザイスクワット",
+		"5回(5回)",
+		"モモあげ",
+		"1m(801m)",
+		"ジョギング",
+		"2m(19809m)",
+	}
+	after := replaceLines(before)
+	for idx, out := range after {
+		want := wants[idx]
+		if out != want {
+			err := errors.Errorf("Fail.\n[out ]: %s\n[want]: %s", out, want)
+			t.Error(err)
+		}
 	}
 }
